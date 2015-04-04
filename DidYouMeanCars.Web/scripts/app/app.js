7ea20b1a666ee376de948f28534c909bb404066d
nuwanns <nuwanns@gmail.com>
2015-04-04 19:09:08 +0530

    added tthe todocontroller

--- DidYouMeanCars.Web/scripts/app/app.js
+++ DidYouMeanCars.Web/scripts/app/app.js
@@ -2,6 +2,7 @@ angular.module('app', ['ngResource', 'ngRoute']);
 
 angular.module('app').config(function($routeProvider, $locationProvider){
     $locationProvider.html5Mode(true);
-    $routeProvider.when('/', { templateUrl: '../../views/partials/home.html', controller: 'homeController'});
+    $routeProvider.when('/', { templateUrl: '../../views/partials/home.html', controller: 'homeController' });
+    $routeProvider.when('/todolist', { templateUrl: '../../views/partials/todoList.html', controller: 'todoListController' });
 });
 
---
+++ DidYouMeanCars.Web/scripts/app/controllers/todoListController.js
@@ -0,0 +1,11 @@
+﻿angular.module('app').controller('todoController', function ($scope, $http) {
+    //TODO get this from config
+    var root = 'http://localhost:1337';
+
+    $http.get(root + '/api/todolist').then(function (result) {
+        $scope.events = result.data;
+    }, function (err) {
+        console.log(err);
+    });
+
+});
