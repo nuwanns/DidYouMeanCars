c8b5efa5d7e46f7d1d1cc857754bc5684c0e6a39
nuwanns <nuwanns@gmail.com>
2015-04-05 23:23:25 +0530

    added the todolist domain events

--- DidYouMeanCars.Api/domain/TodoList.js
+++ DidYouMeanCars.Api/domain/TodoList.js
@@ -4,20 +4,47 @@
         util = require('util');
 
     TodoList = function (id, name) {
-        var self = this;
-        self.id = id;
-        self.name = name;
-        self.isArchived = false;
+        applyChange(new TodoListCreated(id, name));
     };
 
     util.inherits(TodoList, AggregateRoot);
 
-    TodoList.prototype.rename = function(){
-
+    TodoList.prototype.rename = function(id,newName){
+        applyChange(new TodoListRenamed(id, newName));
     };
 
     TodoList.prototype.archive = function(){
+        applyChange(new TodoListArchived(id));
+    };
+    
+    // Event handlers
 
+    TodoList.prototype.applyTodoListCreated = function(event) {
+        this.id = event.id;
+        this.isArchived = false;
+    };
+
+    TodoList.prototype.applyTodoListArchived = function (event) {
+        this.isArchived = true;
+    };
+    
+    // Domain events
+
+    TodoList.prototype.TodoListCreated = function (id, name) {
+        this.type = 'TodoListCreated';
+        this.id = id;
+        this.name = name;
+    };
+    
+    TodoList.prototype.TodoListRenamed = function (id, newName) {
+        this.type = 'TodoListRenamed';
+        this.id = id;
+        this.newName = newName;
+    };
+    
+    TodoList.prototype.TodoListArchived = function (id) {
+        this.type = 'TodoListArchived';
+        this.id = id;
     };
 
 })(module.exports);
--- DidYouMeanCars.Api/infrastructure/AggregateRoot.js
+++ DidYouMeanCars.Api/infrastructure/AggregateRoot.js
@@ -1,28 +1,33 @@
-(function(AggregateRoot){
-
-    AggregateRoot = function(id){
+(function (AggregateRoot) {
+    
+    AggregateRoot = function (id) {
         var self = this;
         self.id = id;
         self.version = 0;
         self.changes = [];
     };
-
-    AggregateRoot.prototype.getUncommittedEvents = function(){
+    
+    AggregateRoot.prototype.getUncommittedEvents = function () {
         return this.changes;
     }
-
-    AggregateRoot.prototype.markChangesAsCommitted = function(){
+    
+    AggregateRoot.prototype.markChangesAsCommitted = function () {
         this.changes.clear();
     }
-
-    AggregateRoot.prototype.loadFromHistory = function(events){
-        events.forEach(function(e){
+    
+    AggregateRoot.prototype.loadFromHistory = function (events) {
+        events.forEach(function (e) {
 
         });
     }
-
-    AggregateRoot.prototype.applyChange = function(event){
-
+    
+    AggregateRoot.prototype.applyChange = function (event, isNew) {
+        if (this.hasOwnProperty('apply' + event.type)) {
+            this['apply' + event.type](event);
+            if (isNew) {
+                changes.push(event);
+            }
+        }      
     }
 
 })(module.exports);
