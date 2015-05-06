be09e83009ac34864f7a552c39defb195931b525
unknown <nsgunawardena@CMLNSGUNAWARDEN.Virtusa.com>
2015-05-06 17:49:46 +0530

    Adding and viewing todolists

--- DidYouMeanCars.Api/controllers/todoListController.js
+++ DidYouMeanCars.Api/controllers/todoListController.js
@@ -5,34 +5,13 @@
         todoListFacade = require('../readModel/todoListFacade');
     
     todoListController.init = function (app) {
-        
-        //app.get("/api/todolist", function(req, res){
-        //    var atomEventReader = require('../services/atomEventReader');
-        //    atomEventReader.getEventsFromStream('http://127.0.0.1:2113/streams/todolist-63e645ea-c577-4bad-a9cd-73dae16df91a', function(events){
-        //        res.set("Content-Type", "application/json");
-        //        res.send(events);
-        //    });
-        //});
-        
-        //app.post("/api/todolist", function(req, res){
-        //    var eventStore = require('../infrastructure/eventStore');
-        //    var events =  [{
-        //            "eventId": uuid.v4(),
-        //            "eventType": "TodoListCreated",
-        //            "data": {
-        //                "name": req.body.name
-        //            }
-        //        }];
-        //    eventStore.save('http://127.0.0.1:2113/streams/todolist-' + uuid.v4(), events);
-        //    res.set("Content-Type", "application/json");
-        //    res.send([{status: 'created'}]);
-        //});
-        
+              
         app.get("/api/todolist", function (req, res) {
             var callback = function (error, results) {
                 res.set("Content-Type", "application/json");
                 if (error) {
-                    res.send(null); // return error
+                    console.log(error);
+                    res.send([{ status: 'error getting data' }]);
                 } else {
                     res.send(results);
                 }
--- DidYouMeanCars.Web/scripts/app/controllers/todoListController.js
+++ DidYouMeanCars.Web/scripts/app/controllers/todoListController.js
@@ -3,14 +3,16 @@
     var root = 'http://localhost:1337';
 
     $http.get(root + '/api/todolist').then(function (result) {
-        $scope.events = result.data;
+        $scope.todoLists = result.data;
     }, function (err) {
         console.log(err);
     });
 
     $scope.save = function (todoList) {
         $http.post(root + '/api/todolist', { name: todoList.name })
-            .success(function (data) { })
+            .success(function (data) {
+                alert('saved');
+            })
             .error(function (data) {
                 console.log(data);
             });
