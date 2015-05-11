f7e52479d15a8346a5eec5a49939fb1a92c968f4
unknown <nsgunawardena@CMLNSGUNAWARDEN.Virtusa.com>
2015-05-11 14:38:11 +0530

    added the fake event store

---
+++ DidYouMeanCars.Api/infrastructure/fakeEventStore.js
@@ -0,0 +1,13 @@
+﻿(function (fakeEventStore) {
+    
+    var events = [];
+
+    fakeEventStore.init = function (given) {
+        events = given;
+    };
+
+    fakeEventStore.peekChanges = function () {
+        return events;
+    };
+
+})(module.exports);
