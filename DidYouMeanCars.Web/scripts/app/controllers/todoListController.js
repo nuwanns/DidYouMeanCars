17919e4df12df2db28598860641863afafa0a3bb
unknown <nsgunawardena@CMLNSGUNAWARDEN.Virtusa.com>
2015-05-11 12:38:20 +0530

    rename UI working

--- DidYouMeanCars.Web/scripts/app/controllers/todoListController.js
+++ DidYouMeanCars.Web/scripts/app/controllers/todoListController.js
@@ -18,65 +18,45 @@
             });
     };
 
-    $scope.selectedTodoList = null;
-
-    $scope.renameTodoList = function (todoList) {
-        $scope.selectedTodoList = todoList;
-        $location.path('/todolist/rename');
-    };
-
-    $scope.rename = function (todoList) {
-        $http.put(root + '/api/todolist', { id : todoList.id, newName: todoList.newName })
-           .success(function (data) {
-               alert('saved');
-           })
-           .error(function (data) {
-               console.log(data);
-           });
-    }
-
-    $scope.items = ['item1', 'item2', 'item3'];
-
-    $scope.animationsEnabled = true;
-
-    $scope.open = function (size) {
-
+    $scope.open = function (todoList) {
         var modalInstance = $modal.open({
-            animation: $scope.animationsEnabled,
-            templateUrl: 'myModalContent.html',
-            controller: 'ModalInstanceCtrl',
-            //windowTemplateUrl: 'modalWindowTemplte.html',
+            animation: true,
+            templateUrl: 'renameTodoList.html',
+            controller: 'renameTodoListController',
             windowClass: 'app-modal-window',
-            //size: size,
-            //backdrop: false,
             resolve: {
-                items: function () {
-                    return $scope.items;
+                selectedTodoList: function () {
+                    return todoList;
                 }
             }
         });
 
-        modalInstance.result.then(function (selectedItem) {
-            $scope.selected = selectedItem;
+        modalInstance.result.then(function (selectedTodoList) {
+            $http.put(root + '/api/todolist', { id: selectedTodoList.id, newName: selectedTodoList.newName })
+            .success(function (data) {
+                alert('saved');
+            })
+            .error(function (data) {
+                console.log(data);
+            });
         }, function () {
-            $log.info('Modal dismissed at: ' + new Date());
+            console.log('Modal dismissed at: ' + new Date());
         });
     };
 
 });
 
-angular.module('app').controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+angular.module('app').controller('renameTodoListController', ['$scope', '$modalInstance', 'selectedTodoList',
+    function ($scope, $modalinstance, selectedTodoList) {
 
-    $scope.items = items;
-    $scope.selected = {
-        item: $scope.items[0]
-    };
+        $scope.selectedTodoList = selectedTodoList;
 
-    $scope.ok = function () {
-        $modalInstance.close($scope.selected.item);
-    };
+        $scope.ok = function () {
+            $modalinstance.close($scope.selectedTodoList);
+        };
 
-    $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
-    };
-});
+        $scope.cancel = function () {
+            $modalinstance.dismiss('cancel');
+        };
+
+    }]);
